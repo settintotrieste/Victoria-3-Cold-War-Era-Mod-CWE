ef04a5f1826dd53122f47c13175b0c0c5dfaf0a5
yt-koike <42059282+yt-koike@users.noreply.github.com>
2025-01-20 20:22:02 +0900

    auto-translate legitimacy

--- localization/translate.py
+++ localization/translate.py
@@ -1,7 +1,45 @@
 from googletrans import Translator
-
-str = "これはバナナです。"  # 翻訳したい文字
+import time
 
 translator = Translator()
-trans_text = translator.translate(str)  # デフォルトでは英語へ変換
-print(trans_text.text)
+
+cache = {}
+
+
+def translate(rawtext) -> str:
+    if rawtext in cache:
+        return cache[rawtext]
+    try:
+        x = int(rawtext.split(" ")[-1])
+        rawtext = " ".join(rawtext.split(" ")[:-1])
+        return translate(rawtext) + " " + str(x)
+    except:
+        pass
+    while True:
+        try:
+            translated = translator.translate(rawtext, "ja", "en")
+            cache[rawtext] = translated.text
+            return translated.text
+        except:
+            print("Error occured. Waiting for 5 secs...")
+            time.sleep(5)
+
+
+dstLang = "japanese"
+fileCommonName = "0_buildings_l"
+filepathIn = f"english\\{fileCommonName}_english.yml"
+filepathOut = f"{dstLang}\\{fileCommonName}_{dstLang}.yml"
+
+with open(filepathIn, "r", encoding="utf-8") as f:
+    with open(filepathOut, "w", encoding="utf-8") as of:
+        for line in f:
+            fields = line.split('"')
+            if len(fields) == 3:
+                rawtext = fields[1]
+                print(rawtext)
+                if len(rawtext.removeprefix(" ")) > 0:
+                    translated = translate(rawtext)
+                    fields[1] = translated
+            newline = '"'.join(fields)
+            print(newline)
+            of.write(newline)
